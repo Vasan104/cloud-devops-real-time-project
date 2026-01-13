e195e5c50f31ceee4cbc8be395d81d30fc27cc2f
Ubuntu <ubuntu@ip-172-31-21-117.ec2.internal>
2026-01-13 08:10:49 +0000

    Commit #4: Integrated Flask application with AWS RDS MySQL

--- app/app.py
+++ app/app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+import pymysql
 
 app = Flask(__name__)
 
+db = pymysql.connect(
+    host="devops-rds.caz0ecyo642l.us-east-1.rds.amazonaws.com",
+    user="admin",
+    password="Vasan10401",
+    database="devopsdb"
+)
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+    return render_template("index.html", users=users)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
---
+++ docs/rds-integration.md
@@ -0,0 +1,17 @@
+# RDS Integration
+
+This document explains how AWS RDS MySQL was integrated with the Flask application.
+
+## RDS Configuration
+- Engine: MySQL
+- Database: devopsdb
+- Connectivity: EC2 Security Group
+- Port: 3306
+
+## Application Integration
+- Installed PyMySQL
+- Connected Flask app to RDS
+- Fetched user data dynamically
+
+## Result
+Application successfully reads data from Amazon RDS.
